6247f3e4a341face9ddda45b01ab3e845a72c4a4
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-06 23:24:24 +0200

    Skeletton for Discover, Track and Share

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,15 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def discover
+  end
+
+  def track
+  end
+
+  def share
+  end
 
   def about
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ VinoSocialis::Application.routes.draw do
   devise_for :users
 
   root :to => "home#index"
+  match '/discover', :to => "home#discover"
+  match '/track', :to => "home#track"
+  match '/share', :to => "home#share"
   match '/about', :to => "home#about"
 end
