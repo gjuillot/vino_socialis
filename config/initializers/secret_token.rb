6f9eb7b3f66493de0fc0fba4ffaa49af505b59c9
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-06 16:30:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VinoSocialis::Application.config.secret_token = '3ae0064dd701f9161f5fac09c3dca57333793b43a64f6a6e5d36e132afd251223c5e25cb040f9d13f262fc05d681f55c50a9e87d363a3fce54c971933a696fbf'
