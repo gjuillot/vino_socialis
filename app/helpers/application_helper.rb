3642bec543dabbc73d79d655796987ed466f9047
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-07 17:57:09 +0200

    Nice design with bootstrap and icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,12 @@ module ApplicationHelper
       "#{site_name} - #{page_title}"
     end
   end
+  
+  def link_to_edit(path)
+    link_to content_tag(:i, "", class: "icon-edit"), path
+  end
+  
+  def link_to_destroy(model)
+    link_to content_tag(:i, "", class: "icon-trash"), model, confirm: 'Are you sure?', method: :delete
+  end
 end
