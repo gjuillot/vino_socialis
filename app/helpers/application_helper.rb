883c94099a9a94c675a63349a9c5d906e33d48ae
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-07 16:42:03 +0200

    Page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def site_name
+    "Vino Socialis"
+  end
+  
+  def title(page_title)
+    if (page_title.empty?)
+      site_name
+    else
+      "#{site_name} - #{page_title}"
+    end
+  end
 end
