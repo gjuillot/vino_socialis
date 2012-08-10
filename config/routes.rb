b72d3476b50ee8d7a8ba1ed4937cb4ed8f2fb9ef
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-10 19:00:28 +0200

    i18n: english and french

--- config/routes.rb
+++ config/routes.rb
@@ -1,42 +1,46 @@
 VinoSocialis::Application.routes.draw do
-
-  resources :tastings
-
-  resources :estates do
-    member do
-      post 'validate'
-      post 'unvalidate'
-    end
-    collection do
-      get 'search'
-    end
-  end
-
-  resources :wines do
-    member do
-      post 'validate'
-      post 'unvalidate'
-      post 'taste'
-    end
-    collection do
-      get 'search'
-    end
-  end
-
-  resources :wine_colors
-
-  resources :countries
-  resources :regions
-  resources :areas
   
-  devise_for :users
-  resources :users
+  scope "/:locale" do
+    resources :tastings
 
+    resources :estates do
+      member do
+        post 'validate'
+        post 'unvalidate'
+      end
+      collection do
+        get 'search'
+      end
+    end
+
+    resources :wines do
+      member do
+        post 'validate'
+        post 'unvalidate'
+        post 'taste'
+      end
+      collection do
+        get 'search'
+      end
+    end
+
+    resources :wine_colors
+
+    resources :countries
+    resources :regions
+    resources :areas
+  
+    devise_for :users
+    resources :users
+
+    match '/discover', :to => "home#discover"
+    match '/track', :to => "home#track"
+    match '/share', :to => "home#share"
+    match '/about', :to => "home#about"
+    match '/wines_and_estates', :to => "wines_and_estates#index"
+  end
+  
+  match "/:locale" => "home#discover"
   root :to => "home#index"
-  match '/discover', :to => "home#discover"
-  match '/track', :to => "home#track"
-  match '/share', :to => "home#share"
-  match '/about', :to => "home#about"
-  match '/wines_and_estates', :to => "wines_and_estates#index"
   
 end
