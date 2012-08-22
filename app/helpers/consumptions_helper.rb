cb8af4cc9e2d51e769412e03d795fffa427c98a0
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-22 10:03:31 +0200

    Improved consumptions display

--- app/helpers/consumptions_helper.rb
+++ app/helpers/consumptions_helper.rb
@@ -1,2 +1,20 @@
 module ConsumptionsHelper
+  
+  def consumption_action_button(consumption)
+      # ACTION LINKS
+    res = '<div class="btn-group">
+              <button class="btn btn-mini dropdown-toggle" data-toggle="dropdown">
+                Actions <span class="caret"></span>
+              </button>
+              <ul class="dropdown-menu">'
+      
+      res += link_to_show(consumption_path(consumption))
+      
+      if can? :manage, consumption
+        res += link_to_edit(edit_consumption_path(consumption))
+      end
+      res += '</ul></div>'
+      
+      return raw res
+  end
 end
