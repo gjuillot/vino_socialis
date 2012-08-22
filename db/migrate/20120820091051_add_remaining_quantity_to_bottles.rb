9adf80f05df9201d40c4b30770472e450deec9a7
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-22 21:20:30 +0200

    Fix db migration file

--- db/migrate/20120820091051_add_remaining_quantity_to_bottles.rb
+++ db/migrate/20120820091051_add_remaining_quantity_to_bottles.rb
@@ -1,6 +1,6 @@
 class AddRemainingQuantityToBottles < ActiveRecord::Migration
   def change
-    remove_column :bottles, :quantity, :integer
+    remove_column :bottles, :quantity
     add_column :bottles, :remaining_quantity, :integer
     add_column :bottles, :initial_quantity, :integer
   end
