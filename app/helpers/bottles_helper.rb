97c88b89284ffdc161264c7c9a7fb7efa3f32820
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-08-23 14:44:52 +0200

    Nice homepage

--- app/helpers/bottles_helper.rb
+++ app/helpers/bottles_helper.rb
@@ -4,4 +4,8 @@ module BottlesHelper
     html ||= options[:html]
     wine_name(bottle.wine, estate: true, area: true, html: html) + " - #{bottle.vintage}"
   end
+  
+  def bottles_count_remaining
+    Bottle.sum(:remaining_quantity)
+  end
 end
