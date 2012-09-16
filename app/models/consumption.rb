b6edb6d343d381ca6ce6e2f6d178ea16cb1733ea
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-09-16 22:43:48 +0200

    Issue #10 : Consumptions: default order by date

--- app/models/consumption.rb
+++ app/models/consumption.rb
@@ -2,5 +2,7 @@ class Consumption < ActiveRecord::Base
   belongs_to :user
   belongs_to :bottle
   
+  default_scope order: 'date DESC'
+  
   validates :quantity, :numericality => { :only_integer => true, :greater_than => 0 }
 end
