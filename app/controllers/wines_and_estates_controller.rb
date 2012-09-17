e7216b8615bf446525aa2b031c5228b4b68b280f
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-09-17 10:30:14 +0200

    Closes #8 : Advanced search of wine sheets

--- app/controllers/wines_and_estates_controller.rb
+++ app/controllers/wines_and_estates_controller.rb
@@ -14,29 +14,54 @@ class WinesAndEstatesController < ApplicationController
     else
       @wines = Wine.order('random()').limit(5)
     end
+    @countries = Country.all
+    @areas = []
+    @display_estate = true
+    @display_wine = true
   end
   
   def search
     if params[:q].empty?
       redirect_to action: 'index'
-    else
-      @searched = params[:q]
+      return
+    end
+    
+    @display_estate = (params[:scope] != 'wine')
+    @display_wine = (params[:scope] != 'estate')
+    @searched = params[:q]
+    
+    if @display_estate
+      @estates = Estate.where("name LIKE ?", "%#{@searched}%").order('name')
       if params[:replaced_estate]
         @replaced_estate = Estate.find(params[:replaced_estate])
-        @estates = Estate.where('"wines".id != ?', @replaced_estate.id).where('"wines".validation = ?' , true).order('random()').limit(5)
-      else
-        @estates = Estate.order('random()').limit(5)
+        @estates = @estates.where('"wines".id != ?', @replaced_estate.id).where('"wines".validation = ?' , true)
       end
+    end
+    
+    if @display_wine
+      @wines = Wine.joins(:estate).where('"wines".name LIKE ? OR "estates".name LIKE ?', "%#{@searched}%", "%#{@searched}%").order('"estates".name, "wines".name')
       if params[:replaced_wine]
         @replaced_wine = Wine.find(params[:replaced_wine])
-        @wines = Wine.where('"wines".id != ?', @replaced_wine.id).where('"wines".validation = ?' , true).order('random()').limit(5)
-      else
-        @wines = Wine.order('random()').limit(5)
+        @wines = @wines.where('"wines".id != ?', @replaced_wine.id).where('"wines".validation = ?' , true)
       end
-      @estates = @estates.where("name LIKE ?", "%#{@searched}%").order('name')
-      @wines = @wines.joins(:estate).where('"wines".name LIKE ? OR "estates".name LIKE ?', "%#{@searched}%", "%#{@searched}%").order('"estates".name, "wines".name')
-      render action: 'index'
+      
+      if params[:scope] == 'wine'
+        if !params[:region][:id].blank? && !params[:area_id].blank?
+          @region_id = params[:region][:id]
+          @area_id = params[:area_id]
+          @region = Region.find(@region_id)
+          @wines = @wines.where('"wines".area_id = ?', @area_id)
+        elsif !params[:region][:id].blank?
+          @region_id = params[:region][:id]
+          @region = Region.find(@region_id)
+          @wines = @wines.joins(:area).where('"areas".region_id = ?', @region_id)
+        end
+      end
     end
+
+    @countries = Country.all
+    @areas = (@region ? @region.areas : [])
+    render action: 'index'
   end
 
 end
