92abdf5241c7bf6dbedfa0bc80b7c340728dd165
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-10-26 18:11:24 +0200

    Fixes #49: Placing non vintaged bottles

--- app/helpers/bottles_helper.rb
+++ app/helpers/bottles_helper.rb
@@ -2,7 +2,7 @@ module BottlesHelper
   
   def bottle_name(bottle, options = {})
     html ||= options[:html]
-    wine_name(bottle.wine, estate: true, area: true, html: html) + " - #{formated_vintage(bottle.vintage)}"
+    wine_name(bottle.wine, estate: true, area: true, html: html) + " - " + formated_vintage(bottle.vintage, false).to_s
   end
   
   def bottles_count_remaining
