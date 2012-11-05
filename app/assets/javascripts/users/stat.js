249356f493cb43224ce916ddacae4438af9c6e22
Guillaume Juillot <guillaume.juillot@gmail.com>
2012-11-05 09:34:22 +0100

    Display "Loading" during ajax call for stats

--- app/assets/javascripts/users/stat.js
+++ app/assets/javascripts/users/stat.js
@@ -1,54 +1,38 @@
 $('.users.stat').ready(function() {
   
-  var areas = false;
-  var colors = false;
-  var vintages = false;
-  var purchase_years = false;
-  var consumptions = false;
+  $('div.progress').hide();
+  
+  var already_done = {};
+  
+  function load_bargraph(name, comparator) {
+    if (!already_done[name]) {
+      $('div.progress').show();
+      d3.json("stat.json?" + name + "=true", function(json) {
+        bargraph(json, 'div#' + name, comparator);
+        $('div.progress').hide();
+      });
+      already_done[name] = true;
+    }
+  }
   
   $('#myTab a[href="#areas"]').click(function() {
-    if (!areas) {
-      d3.json("stat.json?areas=true", function(json) {
-        bargraph(json, 'div#areas', null);
-      });
-      areas = true;
-    }
+    load_bargraph('areas', null);
   });
   
   $('#myTab a[href="#colors"]').click(function() {
-    if (!colors) {
-      d3.json("stat.json?colors=true", function(json) {
-        bargraph(json, 'div#colors', null);
-      });
-      colors = true;
-    }
+    load_bargraph('colors', null);
   });
   
   $('#myTab a[href="#vintages"]').click(function() {
-    if (!vintages) {
-      d3.json("stat.json?vintages=true", function(json) {
-        bargraph(json, 'div#vintages', function(a,b) { return d3.ascending(a.name, b.name);});
-      });
-      vintages = true;
-    }
+    load_bargraph('vintages', function(a,b) { return d3.ascending(a.name, b.name);});
   });
   
   $('#myTab a[href="#purchase_years"]').click(function() {
-    if (!purchase_years) {
-      d3.json("stat.json?purchase_years=true", function(json) {
-        bargraph(json, 'div#purchase_years', function(a,b) { return d3.ascending(a.name, b.name);});
-      });
-      purchase_years = true;
-    }
+    load_bargraph('purchase_years', function(a,b) { return d3.ascending(a.name, b.name);});
   });
   
   $('#myTab a[href="#consumptions"]').click(function() {
-    if (!consumptions) {
-      d3.json("stat.json?consumptions=true", function(json) {
-        bargraph(json, 'div#consumptions', null);
-      });
-      consumptions = true;
-    }
+    load_bargraph('consumptions', null);
   });
   
 });
