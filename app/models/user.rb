e7cb374fe2da1b3698825c2e2ca8d37c46e4887a
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-01-14 08:18:52 +0100

    Remove devise non-working feature (will have to fix them later)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :trackable, :validatable
          
   acts_as_messageable
 
