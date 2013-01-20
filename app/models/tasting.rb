2471a5799fddf6ebdca8121a111af65d57fa63dd
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-01-20 14:07:11 +0100

    Fix Tasting.last scope on Heroku

--- app/models/tasting.rb
+++ app/models/tasting.rb
@@ -2,5 +2,5 @@ class Tasting < ActiveRecord::Base
   belongs_to :wine
   belongs_to :user
   
-  scope :last, lambda {|count| order('"id DESC"').limit(count) }
+  scope :last, lambda {|count| order("id DESC").limit(count) }
 end
