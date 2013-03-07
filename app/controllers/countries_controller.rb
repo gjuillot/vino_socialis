811d80ea16ba432eb1d0f5119ae0f72ce154e527
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-03-07 22:14:44 +0100

    json and xml for countries and regions

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -4,11 +4,21 @@ class CountriesController < ApplicationController
   
   # GET /countries
   def index
+    respond_to do |format|
+      format.html
+      format.json { render :json => @countries }
+      format.xml { render :xml => @countries }
+    end
   end
 
   # GET /countries/1
   def show
     @regions = @country.regions
+    respond_to do |format|
+      format.html
+      format.json { render :json => @country.to_json(:include => [:regions]) }
+      format.xml { render :xml => @country.to_xml(:include => [:regions]) }
+    end
   end
 
 end
--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,11 +4,21 @@ class RegionsController < ApplicationController
   
   # GET /regions
   def index
+    respond_to do |format|
+      format.html
+      format.json { render :json => @regions }
+      format.xml { render :xml => @regions }
+    end
   end
 
   # GET /regions/1
   def show
     @areas = @region.areas
+    respond_to do |format|
+      format.html
+      format.json { render :json => @region.to_json(:include => [:areas]) }
+      format.xml { render :xml => @region.to_xml(:include => [:areas]) }
+    end
   end
 
 end
