07d420d3f83a4a810983b245e74c77b21900ed72
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-05-15 21:44:44 +0200

    Better ergonomy for areas#edit

--- app/controllers/area_color_grapes_controller.rb
+++ app/controllers/area_color_grapes_controller.rb
@@ -3,7 +3,18 @@ class AreaColorGrapesController < ApplicationController
   load_and_authorize_resource
   
    def create
-    @area_color_grape.save
+     params[:main].each_key do |main|
+       acg = AreaColorGrape.new(params[:area_color_grape])
+       acg.grape_variety_id = main.to_i
+       acg.main = true
+       acg.save
+     end
+     params[:second].each_key do |second|
+       acg = AreaColorGrape.new(params[:area_color_grape])
+       acg.grape_variety_id = second.to_i
+       acg.main = false
+       acg.save
+     end
     redirect_to edit_area_path(Area.find(@area_color_grape.area_id))
   end
   
