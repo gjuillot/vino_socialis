d04f104780aefac1124cd1269b7cfa1cff39378d
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-05-16 21:05:50 +0200

    Fix area_color_grape creation when no secondary grape variety

--- app/controllers/area_color_grapes_controller.rb
+++ app/controllers/area_color_grapes_controller.rb
@@ -1,26 +1,28 @@
 class AreaColorGrapesController < ApplicationController
-  
+
   load_and_authorize_resource
   
-   def create
-     params[:main].each_key do |main|
-       acg = AreaColorGrape.new(params[:area_color_grape])
-       acg.grape_variety_id = main.to_i
-       acg.main = true
-       acg.save
-     end
-     params[:second].each_key do |second|
-       acg = AreaColorGrape.new(params[:area_color_grape])
-       acg.grape_variety_id = second.to_i
-       acg.main = false
-       acg.save
-     end
+  def create
+    params[:main].each_key do |main|
+      acg = AreaColorGrape.new(params[:area_color_grape])
+      acg.grape_variety_id = main.to_i
+      acg.main = true
+      acg.save
+    end
+    unless params[:second].nil?
+      params[:second].each_key do |second|
+        acg = AreaColorGrape.new(params[:area_color_grape])
+        acg.grape_variety_id = second.to_i
+        acg.main = false
+        acg.save
+      end
+    end
     redirect_to edit_area_path(Area.find(@area_color_grape.area_id))
   end
-  
-   def update
+
+  def update
     @area_color_grape.update_attributes(params[:area_color_grape])
     redirect_to edit_area_path(Area.find(@area_color_grape.area_id))
   end
-  
+
 end
