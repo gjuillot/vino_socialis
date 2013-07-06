5586d11624b67133674544bf87d0e054c19684d3
Guillaume Juillot <guillaume.juillot@gmail.com>
2013-07-06 06:02:29 +0200

    Remove faulty migration file for #31 books

--- db/migrate/20130705180255_change_description_type_in_books.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDescriptionTypeInBooks < ActiveRecord::Migration
-  def up
-    change_column :books, :description, :text, :limit => 500
-  end
-
-  def down
-    change_column :books, :description, :text
-  end
-end
--- db/migrate/20130706055043_change_types_in_books.rb
+++ db/migrate/20130706055043_change_types_in_books.rb
@@ -1,11 +1,9 @@
 class ChangeTypesInBooks < ActiveRecord::Migration
   def up
-    change_column :books, :description, :text
     change_column :books, :amazon, :text
   end
 
   def down
-    change_column :books, :description, :text
     change_column :books, :amazon, :text
   end
 end
