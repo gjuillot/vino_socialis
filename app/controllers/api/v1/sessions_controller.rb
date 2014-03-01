2fe873a5e58cf2179c1496cf12af40229789f02a
Guillaume JUILLOT <guillaume.juillot@alcatel-lucent.com>
2014-03-01 10:23:44 +0100

    Fix authentication by token

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -9,7 +9,8 @@ class Api::V1::SessionsController < Devise::SessionsController
     render :status => 200,
            :json => { :success => true,
                       :info => "Logged in",
-                      :data => { :auth_token => current_user.authentication_token } }
+                      :data => { :auth_token => current_user.authentication_token,
+                                 :user_id => current_user.id } }
   end
 
   def destroy
